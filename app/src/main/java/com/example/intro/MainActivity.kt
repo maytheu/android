6ad6c975bcf40eebd3b0332a89d2d3ff0130ea6a
i-am-Els <emmanomax33@gmail.com>
2023-01-05 08:54:21 +0100

    changing preview

--- app/src/main/java/com/example/intro/MainActivity.kt
+++ app/src/main/java/com/example/intro/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.intro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -34,10 +35,18 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun ShowNum(num:Int){
+    Text(text = num.toString())
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IntroTheme {
-        Greeting("Android")
+        Column() {
+            Greeting("Android")
+            ShowNum(num = 12)
+        }
     }
 }
