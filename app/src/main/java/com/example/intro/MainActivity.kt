0ef0780719b5558b6c0486e88a6cb137c6d3bb60
i-am-Els <emmanomax33@gmail.com>
2023-01-06 08:59:49 +0100

    adding composoable functions

--- app/src/main/java/com/example/intro/MainActivity.kt
+++ app/src/main/java/com/example/intro/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.intro
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.intro.ui.theme.IntroTheme
@@ -21,35 +23,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IntroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxWidth().padding(all = 10.dp),
-                    color = MaterialTheme.colors.primary
-                ) {
-                    Greeting("Android")
-                }
+               MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(){
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = Color(0xFF546E7A)
+    ) {
+    }
 }
-
+@Preview
 @Composable
-fun ShowNum(num:Int){
-    Text(text = num.toString())
+fun CreateCircle(){
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(45.dp), shape = CircleShape) {
+        Box(contentAlignment = Alignment.Center){
+            Text(text = "tap")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true,)
 @Composable
 fun DefaultPreview() {
     IntroTheme {
-        Column() {
-            Greeting("Android")
-            ShowNum(num = 12)
-        }
+MyApp()
     }
 }
