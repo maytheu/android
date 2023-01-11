44e6236e21f0f50682e43d6edc116f0efb7c7b0b
maytheu <maytheu98@gmail.com>
2023-01-11 09:42:38 +0100

    initial commit -m

---
+++ app/src/main/java/ps/maytheu/tipapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package ps.maytheu.tipapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ps.maytheu.tipapp.ui.theme.TipAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipAppTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp {
+                    Text("Ho")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        content()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TipAppTheme {
+        MyApp {
+            Text("Hi")
+        }
+    }
+}
