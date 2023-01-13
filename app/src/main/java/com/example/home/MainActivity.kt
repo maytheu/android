235d86abc863a684ef118f0d6e4ec6b0216760c1
i-am-Els <emmanomax33@gmail.com>
2023-01-13 10:46:25 +0100

    top content

--- app/src/main/java/com/example/home/MainActivity.kt
+++ app/src/main/java/com/example/home/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.home.ui.theme.HomeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +24,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HomeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                App {
+                    TopHeader()
                 }
             }
         }
@@ -30,14 +33,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun App(content: @Composable () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colors.background
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun TopHeader() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(350.dp), color = Color(0xFF1E2AE3)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.size(80.dp),
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo"
+            )
+            Text(
+                text = "Parrot Home",
+                style = MaterialTheme.typography.h3,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFFFFFFFF)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HomeTheme {
-        Greeting("Android")
+        App {
+            TopHeader()
+        }
     }
 }
