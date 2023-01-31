a03864525e69499266dfaf03c364f48098d05712
maytheu <maytheu98@gmail.com>
2023-01-31 09:28:58 +0100

    creating note entity

--- app/src/main/java/com/maytheu/note/model/Note.kt
+++ app/src/main/java/com/maytheu/note/model/Note.kt
@@ -2,12 +2,25 @@ package com.maytheu.note.model
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.time.LocalDateTime
+import java.util.Date
 import java.util.UUID
 
+@Entity(tableName = "notes_tbl")
 data class Note @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey(false)
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "note_title")
     val title: String,
+
+    @ColumnInfo(name = "note_desc")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now(),
+
+    @ColumnInfo(name = "note_entry_date")
+    val entryDate: Date = Date.from(Instant.now()),
 )
