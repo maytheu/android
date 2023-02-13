71d5524f69ab232659236f11419ab8bf5122ba18
maytheu <maytheu98@gmail.com>
2023-02-13 14:53:00 +0100

    model class and hilt app for di

---
+++ app/src/main/java/com/maytheu/quiz/model/Question.kt
@@ -0,0 +1,3 @@
+package com.maytheu.quiz.model
+
+class Question : ArrayList<QuestionItem>()
---
+++ app/src/main/java/com/maytheu/quiz/model/QuestionItem.kt
@@ -0,0 +1,8 @@
+package com.maytheu.quiz.model
+
+data class QuestionItem(
+    val answer: String,
+    val category: String,
+    val choices: List<String>,
+    val question: String
+)
