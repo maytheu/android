59066581c8f8a039d9b53528f20904541ad82c11
maytheu <maytheu98@gmail.com>
2023-02-13 15:10:24 +0100

    singleton api request in module

--- app/src/main/java/com/maytheu/quiz/di/AppModule.kt
+++ app/src/main/java/com/maytheu/quiz/di/AppModule.kt
@@ -1,11 +1,25 @@
 package com.maytheu.quiz.di
 
+import com.maytheu.quiz.service.QuestionApi
+import com.maytheu.quiz.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun provideQuestionApi(): QuestionApi {
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+            .create(QuestionApi::class.java)
+    }
 }
