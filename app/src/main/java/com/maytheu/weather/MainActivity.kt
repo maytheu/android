1833f1edd1bcd617ba62f149d927127e10fd0f97
maytheu <maytheu98@gmail.com>
2023-02-21 13:03:57 +0100

    seeting up nav

--- app/src/main/java/com/maytheu/weather/MainActivity.kt
+++ app/src/main/java/com/maytheu/weather/MainActivity.kt
@@ -3,42 +3,52 @@ package com.maytheu.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.maytheu.weather.navigation.WeatherNavigation
 import com.maytheu.weather.ui.theme.WeatherTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                    //https://api.openweathermap.org/data/2.5/weather?
-                }
-            }
+            WeatherApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WeatherApp() {
+    WeatherTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            //https://api.openweathermap.org/data/2.5/weather?
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                WeatherNavigation()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WeatherTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/maytheu/weather/navigation/WeatherScreens.kt
@@ -0,0 +1,5 @@
+package com.maytheu.weather.navigation
+
+enum class WeatherScreens {
+    SplashScreen, HomeScreen, AboutScreen, FavouriteScreen, SettingScreen, SearchScreen
+}
