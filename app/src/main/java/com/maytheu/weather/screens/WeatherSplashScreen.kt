30f372c8b64084e2dfa46cdd85bdf50f1115937c
maytheu <maytheu98@gmail.com>
2023-02-22 09:45:31 +0100

    add splash screen and asset

--- app/src/main/java/com/maytheu/weather/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/maytheu/weather/screens/WeatherSplashScreen.kt
@@ -1,10 +1,50 @@
 package com.maytheu.weather.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.maytheu.weather.R
 
 @Composable
 fun WeatherSplashScreen(navController: NavController) {
-    Text(text = "Splash screen")
+
+    Surface(
+        modifier = Modifier
+            .padding(20.dp)
+            .size(400.dp),
+        color = Color.White,
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column(
+            modifier = Modifier.padding(2.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sun), contentDescription = "Sunny",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(100.dp)
+            )
+            Text(
+                text = "Find the sun?", style = MaterialTheme.typography.h5,
+                color = Color.LightGray
+            )
+        }
+    }
 }
