8873ffba2c87bcb22787a4dd2926eb4baab774cb
maytheu <maytheu98@gmail.com>
2023-02-23 09:55:04 +0100

    adding retrofit to connect to internet

---
+++ app/src/main/java/com/maytheu/weather/network/WeatherApi.kt
@@ -0,0 +1,17 @@
+package com.maytheu.weather.network
+
+import com.maytheu.weather.model.Weather
+import com.maytheu.weather.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface WeatherApi {
+    @GET("forecast")
+    suspend fun weatherForecast(
+        @Query("q") query: String,
+        @Query("appid"), appid: String = Constants.API_KEY,
+        @Query("units"), units: String = "imperial",
+    ): Weather
+}
---
+++ app/src/main/java/com/maytheu/weather/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.maytheu.weather.utils
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    const val API_KEY = "263fbbe7f8d0f399aab362e432baa1d0"
+}
