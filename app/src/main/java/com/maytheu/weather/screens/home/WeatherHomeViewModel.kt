09dd3602465c2b21d4f5e89fa9329798c96cec29
maytheu <maytheu98@gmail.com>
2023-02-24 14:34:18 +0100

    connect view model to repo

---
+++ app/src/main/java/com/maytheu/weather/screens/home/WeatherHomeViewModel.kt
@@ -0,0 +1,40 @@
+package com.maytheu.weather.screens.home
+
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.maytheu.weather.data.DataOrException
+import com.maytheu.weather.model.Weather
+import com.maytheu.weather.repository.WeatherRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherHomeViewModel @Inject constructor(private val weatherRepo: WeatherRepo) : ViewModel() {
+    val data: MutableState<DataOrException<Weather, Boolean, Exception>> =
+        mutableStateOf(
+            DataOrException(null, true, Exception(""))
+        )
+
+    init {
+        loadWeather()
+    }
+
+    private fun loadWeather() {
+        getWeather("lagos", "imperial")
+    }
+
+    private fun getWeather(s: String, units:String) {
+        viewModelScope.launch {
+            if (s.isNotEmpty()) return@launch
+
+            data.value.loading = true
+            data.value = weatherRepo.getWeatherForecast(s, units)
+            if (data.value.data.toString().isNotEmpty()) data.value.loading = false
+        }
+        Log.d("TAG", "getWeather: ${data.value.data.toString()}")
+    }
+}
