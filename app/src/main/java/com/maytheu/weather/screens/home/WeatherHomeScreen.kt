e89f6727dab4e2bd29bf93cce35c462174bebc2e
maytheu <maytheu98@gmail.com>
2023-02-28 10:21:59 +0100

    adding app bar

--- app/src/main/java/com/maytheu/weather/screens/home/WeatherHomeScreen.kt
+++ app/src/main/java/com/maytheu/weather/screens/home/WeatherHomeScreen.kt
@@ -1,35 +1,56 @@
 package com.maytheu.weather.screens
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.maytheu.weather.data.DataOrException
 import com.maytheu.weather.model.Weather
 import com.maytheu.weather.screens.home.WeatherHomeViewModel
+import com.maytheu.weather.widgets.WeatherAppBar
 
 @Composable
 fun WeatherHomeScreen(navController: NavController, homeViewModel: WeatherHomeViewModel) {
-    ShowData(homeViewModel)
-}
-
-@Composable
-fun ShowData(viewModel: WeatherHomeViewModel) {
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)
     ) {
-        value = viewModel.getWeather(city = "lagos")
+        value = homeViewModel.getWeather(city = "lagos")
     }.value
 
     if (weatherData.loading == true) {
-        Log.d("TAG", "ShowData: loading ${weatherData.toString()}")
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Log.d("TAG", "ShowData: data ${weatherData.toString()}")
-        Text(text = "View model ${weatherData.data.toString()}")
-
+        WeatherData(weatherData.data!!, navController)
     }
-
 }
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun WeatherData(data: Weather, navController: NavController) {
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = "${data.city.name}, ${data.city.country }",
+            elevation = 5.dp,
+            isMainScreen = false,
+            navController = navController,
+//            icon = Icons.Default.ArrowBack
+        )
+    }) {
+        WeatherContent(data = data)
+    }
+}
+
+@Composable
+fun WeatherContent(data: Weather) {
+    Text(text = data.city.name)
+}
+
+
+
---
+++ app/src/main/java/com/maytheu/weather/widgets/WeatherAppBar.kt
@@ -0,0 +1,68 @@
+package com.maytheu.weather.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherAppBar(
+    title: String = "Lagos",
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {},
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title, style = TextStyle(
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                color = MaterialTheme.colors.onSecondary
+            )
+        },
+        actions = {
+            if (isMainScreen) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "search")
+                }
+
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = "more")
+                }
+            } else Box {}
+        },
+        navigationIcon = {
+            if (icon != null) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = "back",
+                    tint = MaterialTheme.colors.onSecondary,
+                    modifier = Modifier.clickable {
+                        onButtonClicked.invoke()
+                    }
+                )
+            }
+        },
+        backgroundColor = Color.Transparent,
+        elevation = elevation
+    )
+
+}
