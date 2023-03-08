07ccd4fd7b41d50e54be2f932d51aa1731e1980d
maytheu <maytheu98@gmail.com>
2023-03-08 16:31:59 +0100

    about page

--- app/src/main/java/com/maytheu/weather/screens/WeatherAboutScreen.kt
+++ app/src/main/java/com/maytheu/weather/screens/WeatherAboutScreen.kt
@@ -1,10 +1,52 @@
 package com.maytheu.weather.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
+import com.maytheu.weather.R
+import com.maytheu.weather.widgets.WeatherAppBar
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun WeatherAboutScreen(navController: NavController){
-    Text(text = "About screen")
+fun WeatherAboutScreen(navController: NavController) {
+    Scaffold(topBar = {
+        WeatherAppBar(
+            navController = navController, isMainScreen = false, icon = Icons.Default.ArrowBack,
+            title = "About"
+        ) {
+            navController.popBackStack()
+        }
+    }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.about),
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = stringResource(id = R.string.about_detail),
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+    }
 }
