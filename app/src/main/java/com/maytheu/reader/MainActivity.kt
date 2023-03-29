161ced5ddc1f243ac78e30cfc0437ccd491ee7cc
maytheu <maytheu98@gmail.com>
2023-03-29 16:46:21 +0100

    adding hilt  di

--- app/src/main/java/com/maytheu/reader/MainActivity.kt
+++ app/src/main/java/com/maytheu/reader/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.maytheu.reader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +11,33 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import com.maytheu.reader.ui.theme.ReaderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderTheme {
+                //initialize the firestore db
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "Mato"
+                user["lastName"] = "Lee"
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    db.collection("users").add(user).addOnSuccessListener {
+                        Log.d("TAG", "onCreate: ${it.id}")
+                    }.addOnFailureListener{
+                        Log.d("TAG", "onCreate: $it")
+                    }
                     Greeting("Android")
                 }
             }
---
+++ app/src/main/java/com/maytheu/reader/ReaderApplication.kt
@@ -0,0 +1,8 @@
+package com.maytheu.reader
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReaderApplication:Application() {
+}
