1b602a8ea50b4073cd8279656420dd24814732a7
maytheu <maytheu98@gmail.com>
2023-03-30 13:30:16 +0100

    add reader screens enum

--- app/src/main/java/com/maytheu/reader/navigation/ReaderScreens.kt
+++ app/src/main/java/com/maytheu/reader/navigation/ReaderScreens.kt
@@ -1,4 +1,20 @@
 package com.maytheu.reader.navigation
 
 enum class ReaderScreens {
+    SplashScreen, LoginScreen, HomeScreen, SearchScreen, UpdateScreen, StatsScreen, BookDetailsScreen, SignUpScreen;
+
+    companion object {
+        fun fromRoute(route: String): ReaderScreens = when (route?.substringBefore("/")) {
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            HomeScreen.name -> HomeScreen
+            SearchScreen.name -> SearchScreen
+            UpdateScreen.name -> UpdateScreen
+            StatsScreen.name -> StatsScreen
+            BookDetailsScreen.name -> BookDetailsScreen
+            SignUpScreen.name -> SignUpScreen
+            null -> HomeScreen
+            else -> throw java.lang.IllegalArgumentException("$route not recognized")
+        }
+    }
 }
