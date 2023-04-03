99a7a1eb1ec0f0d53a2ee9c7ebaeb62de5b4a854
maytheu <maytheu98@gmail.com>
2023-04-03 13:13:24 +0100

    email form display

--- app/src/main/java/com/maytheu/reader/components/Components.kt
+++ app/src/main/java/com/maytheu/reader/components/Components.kt
@@ -1,12 +1,21 @@
 package com.maytheu.reader.components
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 //logo composable
 @Composable
@@ -16,5 +25,51 @@ fun ReaderLogo(modifier: Modifier = Modifier) {
         modifier = modifier.padding(bottom = 10.dp),
         style = MaterialTheme.typography.h3,
         color = Color.Red.copy(alpha = 0.5f)
+    )
+}
+
+
+@Composable
+fun EmailInput(
+    modifier: Modifier = Modifier,
+    emailState: MutableState<String>,
+    label: String = "Email",
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default,
+) {
+    InputField(
+        valueState = emailState,
+        label = label,
+        enabled = enabled,
+        modifier = modifier,
+        imeAction = imeAction,
+        onAction = onAction
+    )
+}
+
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    label: String,
+    enabled: Boolean,
+    isSingleLine: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default,
+) {
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        label = { Text(text = label) },
+        singleLine = isSingleLine,
+        textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colors.onBackground),
+        modifier = modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxSize(),
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+        keyboardActions = onAction
     )
 }
--- app/src/main/java/com/maytheu/reader/screens/login/LoginScreen.kt
+++ app/src/main/java/com/maytheu/reader/screens/login/LoginScreen.kt
@@ -3,10 +3,10 @@ package com.maytheu.reader.screens.login
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,15 +15,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.maytheu.reader.components.EmailInput
 import com.maytheu.reader.components.ReaderLogo
 
-@Preview
 @Composable
 fun LoginScreen(navController: NavController = NavController(context = LocalContext.current)) {
     Surface(modifier = Modifier.fillMaxSize()) {
@@ -32,10 +31,12 @@ fun LoginScreen(navController: NavController = NavController(context = LocalCont
             verticalArrangement = Arrangement.Top
         ) {
             ReaderLogo()
+
+
         }
     }
 }
-
+@Preview
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun UserForm() {
@@ -56,6 +57,8 @@ fun UserForm() {
                 rememberScrollState()
             ), horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        EmailInput(emailState = email, onAction = KeyboardActions {
+            passwordFocusRequest.requestFocus()
+        })
     }
 }
