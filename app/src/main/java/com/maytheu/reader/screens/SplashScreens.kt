6f5ddab04e5a3458eb810ef36922bc5d1778499d
maytheu <maytheu98@gmail.com>
2023-04-03 10:31:03 +0100

    animation on splash

--- app/src/main/java/com/maytheu/reader/screens/SplashScreens.kt
+++ app/src/main/java/com/maytheu/reader/screens/SplashScreens.kt
@@ -1,5 +1,8 @@
 package com.maytheu.reader.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -7,22 +10,39 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
 
-@Preview
 @Composable
 fun SplashScreen(navController: NavController = NavController(context = LocalContext.current)) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+    //show animation
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(targetValue = 0.9f, animationSpec = tween(
+            durationMillis = 900, easing = {
+                OvershootInterpolator(4f).getInterpolation(it)
+            }
+        ))
+        delay(3000L)
+    }
+
     Surface(
         modifier = Modifier
             .padding(20.dp)
-            .size(400.dp),
+            .size(400.dp)
+            .scale(scale.value),
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(width = 2.dp, color = Color.LightGray)
