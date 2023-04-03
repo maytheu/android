cde4d3f99c710193be044f54736da34b3e766af4
maytheu <maytheu98@gmail.com>
2023-04-03 11:59:17 +0100

    login form variables

--- app/src/main/java/com/maytheu/reader/screens/login/LoginScreen.kt
+++ app/src/main/java/com/maytheu/reader/screens/login/LoginScreen.kt
@@ -1,17 +1,23 @@
 package com.maytheu.reader.screens.login
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -30,3 +36,26 @@ fun LoginScreen(navController: NavController = NavController(context = LocalCont
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun UserForm() {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+
+    Column(
+        modifier = Modifier
+            .height(250.dp)
+            .background(MaterialTheme.colors.background)
+            .verticalScroll(
+                rememberScrollState()
+            ), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
