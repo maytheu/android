785bc45acaee54dc9ce5c9ffda2382b725734b8d
maytheu <maytheu98@gmail.com>
2023-04-25 17:15:21 +0100

    cleaning up home

--- app/src/main/java/com/maytheu/reader/components/Components.kt
+++ app/src/main/java/com/maytheu/reader/components/Components.kt
@@ -1,5 +1,7 @@
 package com.maytheu.reader.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -7,10 +9,8 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -26,10 +27,15 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
 import com.google.firebase.auth.FirebaseAuth
+import com.maytheu.reader.model.Book
 import com.maytheu.reader.navigation.ReaderScreens
 
 //logo composable
@@ -163,6 +169,29 @@ fun TitleSection(modifier: Modifier = Modifier, label: String) {
     }
 }
 
+
+
+@Composable
+fun BookRating(score: Double) {
+    Surface(
+        modifier = Modifier
+            .padding(5.dp)
+            .height(70.dp),
+        shape = RoundedCornerShape(60.dp),
+        elevation = 5.dp,
+        color = Color.White
+    ) {
+        Column(modifier = Modifier.padding(5.dp)) {
+            Icon(
+                imageVector = Icons.Filled.StarBorder,
+                contentDescription = "Star icon",
+                modifier = Modifier.padding(2.dp)
+            )
+
+            Text(text = score.toString(), style = MaterialTheme.typography.subtitle1)
+        }
+    }
+}
 @Composable
 fun ReaderAPPBar(title: String, navController: NavController, showProfile: Boolean = true) {
     TopAppBar(
@@ -219,4 +248,128 @@ fun FABContent(onTap: () -> Unit) {
         )
     }
 
+}
+
+@Composable
+fun BookCard(
+    book: Book = Book(id = "1", authors = "me", title = "Potter"),
+    onPressDetials: (String) -> Unit = {},
+) {
+    //screen size info
+    val context = LocalContext.current
+    val resources = context.resources
+    val screen = resources.displayMetrics
+    val screenWidth = screen.widthPixels / screen.density
+
+    val spacing = 10.dp
+
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current).data("https://robohash.org/test.jpg")
+            .crossfade(true).size(Size.ORIGINAL).build()
+    )
+    Card(
+        modifier = Modifier
+            .height(260.dp)
+            .width(202.dp)
+            .padding(20.dp)
+            .clickable { onPressDetials.invoke(book.id.toString()) },
+        elevation = 10.dp,
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(29.dp)
+    ) {
+        Column(
+            modifier = Modifier.width(screenWidth.dp - (spacing * 2)),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Row(horizontalArrangement = Arrangement.Center) {
+//                AsyncImage(
+//                    model = ImageRequest.Builder(LocalContext.current)
+//                        .data("https://robohash.org/test.jpg")
+//                        .transformations(CircleCropTransformation()).build(),
+//                    contentDescription = "Book image",
+//                    modifier = Modifier
+//                        .height(150.dp)
+//                        .width(100.dp)
+//                        .padding(5.dp)
+//                )
+
+
+                Image(
+                    painter = painter,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .padding(4.dp)
+                )
+
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Column(
+                    modifier = Modifier.padding(top = 25.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Favorite,
+                        contentDescription = "Fav icon",
+                        modifier = Modifier.padding(bottom = 2.dp)
+                    )
+
+
+
+                    BookRating(score = 3.5)
+                }
+            }
+
+            Text(
+                text = book.title.toString(),
+                modifier = Modifier.padding(5.dp),
+                fontWeight = FontWeight.Bold,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Text(
+                text = book.authors.toString(),
+                modifier = Modifier.padding(5.dp),
+                style = MaterialTheme.typography.caption
+            )
+
+
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            CardButtonRounded(radius = 70)
+        }
+    }
+
+
+}
+
+
+@Composable
+fun CardButtonRounded(label: String = "button", radius: Int = 30, onClicked: () -> Unit = {}) {
+    Surface(
+        modifier = Modifier.clip(
+            RoundedCornerShape(
+                bottomEndPercent = radius, topStartPercent = radius
+            )
+        ),
+        color = Color(0XFF92CBDF)
+    ) {
+        Column(
+            modifier = Modifier
+                .width(90.dp)
+                .heightIn(40.dp)
+                .clickable { onClicked.invoke() },
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = label, style = TextStyle(Color.White, fontSize = 16.sp))
+        }
+    }
 }
--- app/src/main/java/com/maytheu/reader/screens/home/HomeScreen.kt
+++ app/src/main/java/com/maytheu/reader/screens/home/HomeScreen.kt
@@ -35,9 +35,7 @@ import coil.request.ImageRequest
 import coil.size.Size
 import coil.transform.CircleCropTransformation
 import com.google.firebase.auth.FirebaseAuth
-import com.maytheu.reader.components.FABContent
-import com.maytheu.reader.components.ReaderAPPBar
-import com.maytheu.reader.components.TitleSection
+import com.maytheu.reader.components.*
 import com.maytheu.reader.model.Book
 import com.maytheu.reader.navigation.ReaderScreens
 
@@ -92,9 +90,7 @@ fun HomeContent(navController: NavController = NavController(LocalContext.curren
 
             }
         }
-
         BookCard()
-
     }
 }
 
@@ -103,148 +99,4 @@ fun HomeContent(navController: NavController = NavController(LocalContext.curren
 fun ReadingArea(book: List<Book>, navController: NavController) {
 }
 
-@Preview
-@Composable
-fun BookCard(
-    book: Book = Book(id = "1", authors = "me", title = "Potter"),
-    onPressDetials: (String) -> Unit = {},
-) {
-    //screen size info
-    val context = LocalContext.current
-    val resources = context.resources
-    val screen = resources.displayMetrics
-    val screenWidth = screen.widthPixels / screen.density
 
-    val spacing = 10.dp
-
-    val painter = rememberAsyncImagePainter(
-        model = ImageRequest.Builder(LocalContext.current).data("https://robohash.org/test.jpg")
-            .crossfade(true).size(Size.ORIGINAL).build()
-    )
-    Card(
-        modifier = Modifier
-            .height(250.dp)
-            .width(200.dp)
-            .padding(20.dp)
-            .clickable { onPressDetials.invoke(book.id.toString()) },
-        elevation = 10.dp,
-        backgroundColor = Color.White,
-        shape = RoundedCornerShape(40.dp)
-    ) {
-        Column(
-            modifier = Modifier.width(screenWidth.dp - (spacing * 2)),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Row(horizontalArrangement = Arrangement.Center) {
-//                AsyncImage(
-//                    model = ImageRequest.Builder(LocalContext.current)
-//                        .data("https://robohash.org/test.jpg")
-//                        .transformations(CircleCropTransformation()).build(),
-//                    contentDescription = "Book image",
-//                    modifier = Modifier
-//                        .height(150.dp)
-//                        .width(100.dp)
-//                        .padding(5.dp)
-//                )
-
-
-                Image(
-                    painter = painter,
-                    contentDescription = "",
-                    modifier = Modifier
-                        .height(150.dp)
-                        .width(100.dp)
-                        .padding(5.dp)
-                )
-
-                Spacer(modifier = Modifier.width(10.dp))
-
-                Column(
-                    modifier = Modifier.padding(top = 30.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(
-                        imageVector = Icons.Rounded.Favorite,
-                        contentDescription = "Fav icon",
-                        modifier = Modifier.padding(bottom = 2.dp)
-                    )
-
-
-
-                    BookRating(score = 3.5)
-                }
-            }
-
-            Text(
-                text = book.title.toString(),
-                modifier = Modifier.padding(5.dp),
-                fontWeight = FontWeight.Bold,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-
-            Text(
-                text = book.authors.toString(),
-                modifier = Modifier.padding(5.dp),
-                style = MaterialTheme.typography.caption
-            )
-
-            Row(
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.Bottom
-            ) {
-                CardButtonRounded()
-            }
-        }
-
-
-    }
-
-
-}
-
-@Composable
-fun BookRating(score: Double) {
-    Surface(
-        modifier = Modifier
-            .padding(5.dp)
-            .height(70.dp),
-        shape = RoundedCornerShape(60.dp),
-        elevation = 5.dp,
-        color = Color.White
-    ) {
-        Column(modifier = Modifier.padding(5.dp)) {
-            Icon(
-                imageVector = Icons.Filled.StarBorder,
-                contentDescription = "Star icon",
-                modifier = Modifier.padding(2.dp)
-            )
-
-            Text(text = score.toString(), style = MaterialTheme.typography.subtitle1)
-        }
-    }
-}
-
-@Composable
-fun CardButtonRounded(label: String = "button", radius: Int = 30, onClicked: () -> Unit = {}) {
-    Surface(
-        modifier = Modifier.clip(
-            RoundedCornerShape(
-                bottomEndPercent = radius, topStartPercent = radius
-            )
-        ),
-        color = Color(0XFF92CBDF)
-    ) {
-        Column(
-            modifier = Modifier
-                .width(90.dp)
-                .heightIn(40.dp)
-                .clickable { onClicked.invoke() },
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = label, style = TextStyle(Color.White, fontSize = 16.sp))
-        }
-    }
-}
