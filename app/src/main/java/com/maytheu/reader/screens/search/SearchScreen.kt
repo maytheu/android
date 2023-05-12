779655806c9bb0fda7bf833622af30ed96b643b3
maytheu <maytheu98@gmail.com>
2023-05-13 00:18:37 +0100

    dummy lazy col in search

--- app/src/main/java/com/maytheu/reader/screens/search/SearchScreen.kt
+++ app/src/main/java/com/maytheu/reader/screens/search/SearchScreen.kt
@@ -1,27 +1,36 @@
 package com.maytheu.reader.screens.search
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImagePainter.State.Empty.painter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
 import com.maytheu.reader.components.InputField
 import com.maytheu.reader.components.ReaderAPPBar
+import com.maytheu.reader.model.Book
 import com.maytheu.reader.navigation.ReaderScreens
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -39,9 +48,76 @@ fun SearchScreen(navController: NavController) {
     }) {
         Surface() {
             Column {
-                SearchForm(modifier = Modifier.fillMaxWidth().padding(20.dp))
+                SearchForm(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                ) {
+
+                }
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                BookList(navController)
             }
         }
+    }
+}
+
+@Composable
+fun BookList(navController: NavController) {
+    val testBook = listOf<Book>(
+        Book(title = "test1", notes = "testtest1"),
+        Book(title = "test2", notes = "testtest2"),
+        Book(title = "test3", notes = "testtest3"),
+        Book(title = "test4", notes = "testtest4")
+    )
+
+    LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(16.dp)) {
+        items(items = testBook) { book ->
+            BookRow(book = book, navController)
+        }
+    }
+}
+
+@Composable
+fun BookRow(book: Book, navController: NavController) {
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current).data("https://robohash.org/test.jpg")
+            .crossfade(true).size(Size.ORIGINAL).build()
+    )
+
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .height(120.dp)
+            .fillMaxWidth()
+            .clickable { },
+        shape = RectangleShape,
+        elevation = 6.dp
+    ) {
+        Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.Top) {
+            Image(
+                painter = painter,
+                contentDescription = "",
+                modifier = Modifier
+                    .height(150.dp)
+                    .width(80.dp)
+                    .padding(end = 4.dp)
+            )
+
+            Column() {
+                Text(text = book.title.toString(), overflow = TextOverflow.Ellipsis)
+                Text(
+                    text = "Author: ${book.authors}",
+                    overflow = TextOverflow.Clip,
+                    style = MaterialTheme.typography.caption
+                )
+                //TODO add ther fields
+            }
+
+        }
+
     }
 }
 
