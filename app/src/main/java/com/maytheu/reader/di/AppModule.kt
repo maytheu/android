e64e6ecc6f660fb5d45b4acc069f80032df09829
maytheu <maytheu98@gmail.com>
2023-05-15 11:26:00 +0100

    create retrofit object

--- app/src/main/java/com/maytheu/reader/di/AppModule.kt
+++ app/src/main/java/com/maytheu/reader/di/AppModule.kt
@@ -1,10 +1,25 @@
 package com.maytheu.reader.di
 
+import com.maytheu.reader.network.GoogleBooksApi
+import com.maytheu.reader.utils.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideGoogleBooksApi(): GoogleBooksApi {
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+            .create(GoogleBooksApi::class.java)
+    }
 }
