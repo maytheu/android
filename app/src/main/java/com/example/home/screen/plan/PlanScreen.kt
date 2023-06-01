072ee8fe6d6f4cfe8f1f7c10f972c71a1ae9b1db
maytheu <maytheu98@gmail.com>
2023-06-01 18:00:58 +0100

    show device on floor

--- app/src/main/java/com/example/home/screen/plan/PlanScreen.kt
+++ app/src/main/java/com/example/home/screen/plan/PlanScreen.kt
@@ -1,25 +1,29 @@
 package com.example.home.screen.plan
 
+import android.graphics.Bitmap
+import android.graphics.Path.FillType
 import android.util.Log
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.view.ViewGroup
+import android.webkit.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.produceState
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.PathParser
 import androidx.navigation.NavController
 import com.example.home.component.Expandable
 import com.example.home.component.Layout
@@ -30,6 +34,8 @@ import com.example.home.model.UserDb
 import com.example.home.screen.floor.FloorViewModel
 import com.example.home.screen.home.HomeViewModel
 import com.example.home.utils.Progress
+import com.google.gson.JsonArray
+import org.json.JSONArray
 
 @Composable
 fun PlanScreen(
@@ -69,31 +75,55 @@ fun ShowDevicesOnFloor(
     } else {
         val devices =
             floorViewModel.devices.value.data?.response?.filter { d -> d.floorPlanId == urlParams[0] }
-        DeviceCard(devices?.get(0)?.devices!!, floorId = urlParams[0], planViewModel)
+        DeviceCard(devices?.get(0)?.devices!!, floorId = urlParams[0], planViewModel, navController)
     }
 
 }
 
 @Composable
-fun DeviceCard(devices: List<Device>? = null, floorId: String, planViewModel: PlanViewModel) {
+fun DeviceCard(
+    devices: List<Device>? = null,
+    floorId: String,
+    planViewModel: PlanViewModel,
+    navController: NavController,
+) {
     if (!devices.isNullOrEmpty()) {
+        val phoneDims = LocalContext.current.resources.displayMetrics
+        val sdkLoadingState = remember { mutableStateOf(false) }
         Column {
-            //TODO show webview
-            val tempKey =
-                produceState<Progress<TempKey, Boolean, Exception>>(
-                    initialValue = Progress(
-                        TempKey(
-                            ""
-                        ), true, Exception("")
+            val tempKey = produceState<Progress<TempKey, Boolean, Exception>>(
+                initialValue = Progress(
+                    TempKey(
+                        ""
+                    ), true, Exception("")
+                )
+            ) {
+                value = planViewModel.getTempKey()
+            }.value
+
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(phoneDims.heightPixels.dp.times(0.15f)),
+                color = Color(0xFFEEF1EF),
+                shape = RoundedCornerShape(20.dp),
+            ) {
+                if (tempKey.loading == true) {
+                    Log.d("TAG", "DeviceCard: loading")
+                } else {
+//                    ArchilogicSdk(floorId, key = tempKey.data?.authorization!!)
+                    LoadFpeSdk(
+                        floorPlanId = floorId,
+                        key = tempKey.data?.authorization!!,
+                        sdkLoadingState,
+                        devices,
+                        navController
                     )
-                ) {
-                    value = planViewModel.getTempKey()
-                }.value
+                }
+            }
 
-            Log.d("TAG", "DeviceCard: $tempKey")
+            Spacer(modifier = Modifier.height(10.dp))
 
-            ArchilogicSdk(floorId, key = "123333")
-            Spacer(modifier = Modifier.height(25.dp))
             Surface(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -102,8 +132,7 @@ fun DeviceCard(devices: List<Device>? = null, floorId: String, planViewModel: Pl
                 shape = RoundedCornerShape(20.dp),
             ) {
                 LazyColumn(
-                    modifier = Modifier.padding(2.dp),
-                    contentPadding = PaddingValues(1.dp)
+                    modifier = Modifier.padding(2.dp), contentPadding = PaddingValues(1.dp)
                 ) {
                     items(items = devices) { device ->
                         DeviceExpandableCard(device)
@@ -118,7 +147,6 @@ fun DeviceCard(devices: List<Device>? = null, floorId: String, planViewModel: Pl
 @Composable
 fun DeviceExpandableCard(device: Device) {
     val phoneDims = LocalContext.current.resources.displayMetrics
-
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -161,7 +189,6 @@ fun AttributeCard(att: Attribute) {
 
 @Composable
 fun ArchilogicSdk(floorId: String, key: String) {
-    val webViewHolder = remember { WebViewHolder() }
 
     AndroidView(factory = { context ->
         WebView(context).apply {
@@ -174,8 +201,8 @@ fun ArchilogicSdk(floorId: String, key: String) {
 // Inject JavaScript code to pass the floorId to the floor engine
             webViewClient = object : WebViewClient() {
                 override fun onPageFinished(view: WebView?, url: String?) {
-                    val javascriptCode = evaluateArchilogicCode(floorId)
-                    webViewHolder.webView?.evaluateJavascript(javascriptCode, null)
+                    val javascriptCode = evaluateArchilogicCode(floorId, key)
+                    evaluateJavascript(javascriptCode, null)
 
                     // Load fpe.js script
 //                    evaluateJavascript(
@@ -195,27 +222,24 @@ fun ArchilogicSdk(floorId: String, key: String) {
 //            loadData("<html></html>", "text/html", "UTF-8")
 
             // Load the external JavaScript SDK
-            makeHTML()?.let {
-                loadDataWithBaseURL(
-                    "https://code.archilogic.com/fpe-sdk/v3.1.x/fpe.js",
-                    it, "text/html", "UTF-8", null
-                )
-            }
 
-            // Store the WebView instance in the WebViewRef
-            webViewHolder.webView = this
+            loadDataWithBaseURL(
+                "https://code.archilogic.com/fpe-sdk/v3.1.x/fpe.js",
+                makeHTML(),
+                "text/html",
+                "UTF-8",
+                null
+            )
+
+
         }
     })
 
-//    LaunchedEffect(floorId) {
-//        val javascriptCode = evaluateArchilogicCode(floorId)
-//        evaluateJavaScript(javascriptCode)
-//    }
 }
 
 
-private fun evaluateArchilogicCode(floorId: String): String {
-    Log.d("TAG", "evaluateArchilogicCode: $floorId")
+private fun evaluateArchilogicCode(floorId: String, token: String, height: Int = 100): String {
+    Log.d("TAG", "evaluateArchilogicCode: $floorId $token")
 //    val floorCtrl = "window.floorEngine.load('$floorId');"
 //
 //    Log.d("TAG", "evaluateArchilogicCode: ")
@@ -226,14 +250,13 @@ private fun evaluateArchilogicCode(floorId: String): String {
         function onScriptLoaded() {
         // Create publishable access token at https://app.archilogic.com/team/settings/access-tokens
         const publishableToken = 'd3860d6c-e26e-4934-bd5e-d2f7f707e077';
-        const demoSceneId = '$floorId';
+//        const height = '$height';
 
-        const container = document.createElement('div');
-        container.id = 'hello-plan';
-        document.body.appendChild(container);
+        const container = document.getElementById('hello-plan');
+        document.getElementById('hello-plan').style.height = '$height'
 
         const floorPlan = new FloorPlanEngine(container);
-        floorPlan.loadScene('$floorId', { authorization:"AL-Temp-Token 8C1eJEnKUDit1AqspNLK98eJnDWdKcWUImRY6NJfVQYOtsBsRBMJaX4s/C8rwz2lzAIRDINBF7LUOvPqcR6TGrVUF7h63x6jSwpa7W0KOFipey4+8bwFCuhAMl5mA0NADNEJFHRtXcYg8PTNn5UHKKkPCSt7LiWy6e6AcYShxjTxla2pIWfS0+dUJV8b60+PvcWdYwN/N7hpjSMyCzZOjmjQTuLI65Ypd/D8U5IuRpxPfFGV9ZnFeUlsIC9pFqapaVZeK0VM0nLIwukEB1Z0choub7KhJ2tiq/ZagmcoToHIhJ84AgY/3an5ZVSoGfHVDAfulvEQItnGO0lg76YMbBaxr0luDrLrnhQ1Ipcjkiv4rBlZ21TmxTk41v09mPsJRUnG7sHiA/kazSj7CmYaSAmK+Wn63S/S8VcrIp4l6jRsKHFDQLGMBPximKhXh6QUx/HSvdFzdLZ4+Nc75+c0oETeam6Kl4fC1dLboxLHJwPP5FsbdbkcaMD1tWBRQxhrelIdesCaDFitRf4k0kBWGL/FBbiGX7Xu3HEge3R5MkXjOaKI2xe9wxGLaahHpk+867YcAOo1YNAwE8g7lBGkRZsVS3JnCU4KYvlaCrjmkc41j6ZPel9rV/KhreqUTGy8dz2p818MA3qKtA6uAQgNTbMJ5xsTQidGw0Ex4LvWd8iH4vBop1KslZWyyg8wZ9KVjTObeE8GGcMZEoZRGkbC2g==" });
+        floorPlan.loadScene('$floorId', { authorization:'$token' });
     }
     onScriptLoaded();
     """.trimIndent()
@@ -244,12 +267,14 @@ class WebViewHolder {
 }
 
 
-private fun makeHTML(): String? {
+private fun makeHTML(): String {
     val sb = StringBuilder()
     sb.append("<!DOCTYPE html>\n")
     sb.append("<html>\n")
     sb.append("<head>\n")
     sb.append("<script src=\"https://code.archilogic.com/fpe-sdk/v3.1.x/fpe.js\"></script>\n")
+    sb.append("<style>\n")
+    sb.append("</style>\n")
     sb.append("</head>\n")
     sb.append("<body>\n")
     sb.append("<div id=\"hello-plan\"></div>\n")
@@ -257,3 +282,98 @@ private fun makeHTML(): String? {
     sb.append("</html>\n")
     return sb.toString()
 }
+
+@Composable
+fun LoadFpeSdk(
+    floorPlanId: String,
+    key: String,
+    sdkLoadingState: MutableState<Boolean>,
+    devices: List<Device>,
+    navController: NavController,
+) {
+    val token = key.replace("+", "PARROT")
+    val context = LocalContext.current
+
+    AndroidView(factory = {
+        WebView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+            )
+
+            webViewClient = WebViewClient()
+            // to play video on a web view
+            settings.javaScriptEnabled = true
+
+            // to verify that the client requesting your web page is actually your Android app.
+            settings.userAgentString =
+                System.getProperty("http.agent") //Dalvik/2.1.0 (Linux; U; Android 11; M2012K11I Build/RKQ1.201112.002)
+
+            settings.useWideViewPort = true
+
+            // Bind JavaScript code to Android code
+//            addJavascriptInterface(FloorPlanCallback(), "Android")
+
+
+            webViewClient = object : WebViewClient() {
+                override fun onReceivedError(
+                    view: WebView?,
+                    request: WebResourceRequest?,
+                    error: WebResourceError?,
+                ) {
+                    super.onReceivedError(view, request, error)
+                    Log.d("test001", "error")
+                }
+
+                override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                    sdkLoadingState.value = true
+//                    openFullDialogCustom.value = true
+//                    backEnabled = view.canGoBack()
+                    Log.d("TAG", "onPageStarted: paget started loading")
+                }
+
+                // Compose WebView Part 7 | Hide elements from web view
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    Log.d("TAG", "onPageFinished: page finishe")
+                    super.onPageFinished(view, url)
+
+                    val devicesPos = devices.map { device ->
+                        "{ \"lat\": \"${device.position.lat}\", " +
+                                "\"lng\": \"${device.position.lng}\", " +
+                                "\"notification\": \"${device.notificationDue}\", " +
+                                " \"id\": \"${device.deviceId}\", " +
+                                "\"svg\": \"${device.sensorIcon}\"," +
+                                "\"name\":${device.deviceName} }"
+                    }
+                    val deviceStr = "[${devicesPos.joinToString(",")}]"
+                    val sensorIcons = devices.map { svg -> svg.sensorIcon }
+//                    val sensorIconsStr =
+
+                        Log.d("TAG", "onPageFinished: $deviceStr")
+
+                    evaluateJavascript(
+                        "loadFpeSdk('$floorPlanId', '$key', '$deviceStr', '$sensorIcons')", null
+                    )
+                    sdkLoadingState.value = false
+
+//                    FloorPlanCallback
+//                    openFullDialogCustom.value = false
+//                    removeElement(view!!)
+                }
+
+
+            }
+
+            loadUrl("file:///android_asset/floor.html")
+
+
+        }
+    })
+
+    if (sdkLoadingState.value) {
+        CircularProgressIndicator()
+    }
+}
+
+
+
+
